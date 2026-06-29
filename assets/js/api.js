15939b16cd8525986ed02b9ffd75958c39c9ec08
WTC-Class <dharmatstudy@gmail.com>
2026-06-29 11:04:03 +0530

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 const WTC_API = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbycC3eXM_-NpXYzQ1Vg3MDSvRMUcos5mYBlW62X07fiVc6Gpa4UY7An0e2Shp1f-PCVvQ/exec', // Paste Google Apps Script Web App URL here after deployment.
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyFYi12K1HygIei4EiQuU6ndfnskAqtDJ4676ANQGQ5dR8rkW8B5ZdBU-wW659PFnXrZw/exec', // Paste Google Apps Script Web App URL here after deployment.
   async call(action, payload={}){
     if(!this.WEB_APP_URL){
       console.warn('API URL missing. Using local fallback where available.', action, payload);
